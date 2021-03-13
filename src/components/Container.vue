14e39bb37c32ec32d60d3be0ac2e4014d3959fed
sedatyigit <gnctrkcll947@gmail.com>
2021-03-13 13:43:35 +0300

    düzenleme yapıldı

---
+++ src/components/Container.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <slot/>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.container{
+    width: 940px;
+    margin-left: auto;
+    margin-right: auto;
+
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="header">
+    <Container class="header-inner">
+      <div class="logo">
+          <router-link to="/">ISTAGRAM</router-link> 
+      </div>
+      <form class="input">
+          <input type="text" placeholder="Search">
+      </form>
+    <nav id="nav">
+      <router-link to="/">Home</router-link> 
+      <router-link to="/about">Direct</router-link>
+      <router-link to="/about">Explore</router-link>
+      <router-link to="/about">Profile</router-link>
+    </nav>
+    </Container>
+  </header>
+</template>
+<script>
+import Container from "@/components/Container"
+export default {
+    name : "Header",
+    components : {
+        Container
+    }
+}
+</script>
+<style lang="scss" scoped>
+.header{
+    position: fixed;
+    height: 70px;
+    background: yellow;
+    top: 0;
+    left: 0;
+    right: 0;
+
+    .header-inner{
+        display: grid;
+        grid-auto-columns: 1fr 1fr 1fr;
+        align-items: center;
+    }
+
+}
+    
+</style>
